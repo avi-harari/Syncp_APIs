f80831599214131758e25874e886ab2246537e66
harariboy <harariboy@gmail.com>
2018-04-27 07:20:59 -0400

    Updated usage function

--- GroupAPIs.sh
+++ GroupAPIs.sh
@@ -2,67 +2,38 @@
 
 function usage () {
         echo
-        echo "Usage: ./UserAPIs.sh -o [Option] -u Username -f \"First Name\" -l \"Last Name\" -p Password -e Email -t Type -r Role -d [0/1] -a [0/1] "
+        echo "Usage: ./GroupAPIs.sh -o [Option] -u Username -g \"Group Name\""
         echo
         echo "Options:"
         echo "-o - options are:"
         echo
-        echo "show-users - Show all users."
-        echo "create-user - Create new user. Must input username and type."
-        echo "delete-user - Delete user."
-        echo "show-user - Show user details."
-        echo "edit-user - Edit single user. Enable/Disable user. Activate/Deactivate user. Change email (with -e), first or last name, password or role."
-        echo "edit-users - Edit multiple users. Can't enable/disable or activate/deactivate. Can change account type."
-        echo "company-details - Show company details."
+        echo "get-all-groups - Show all groups."
+        echo "get-user-groups - Get groups of certain user."
+        echo "delete-group - Delete group."
+        echo "get-group-members - Get all members of a certain group."
+        echo "get-group-member - Check if user is a member of a group."
+        echo "delete-from-group - Remove user from a certain group."
+        echo "add-to-group - Add user to a certain group."
         echo
         echo "-u - Username (email)."
-        echo "-f - First Name. If the name has spaces it must be inside double quotes."
-        echo "-l - Last Name. If the name has spaces it must be inside double quotes."
-        echo "-p - Password for account."
-        echo "-e - Email for account."
-        echo "-t - Type of account. Account types are:"
-        echo
-        echo "                3 - PaidBusiness: A user who is subscribed to Syncplicity as part of a business account."
-        echo "                7 - LimitedBusiness: users who have been added to a company account but not yet provided their personal details."
-        echo "               14 - TrialBusiness: A user who is subscribed to Syncplicity as part of a business trial account and hasn't previous subscription."
-        echo "               16 - PendingBusiness: A user who is suggested to be added to a company account, but not yet approved by a company administrator."
-        echo
-        echo "-r - Role of account. Possible users roles:"
-        echo "                1 - AccountOwner (Global administrator and account owner)."
-        echo "                2 - Administrator (Global administrator)."
-        echo "                3 - Support (Support tools)."
-        echo "                4 - ReportViewer (Report viewer)."
-        echo "                5 - SupportAdmin (Support administrator)."
-        echo "                6 - EDiscoveryAdmin (E-Discovery administrator)."
-        echo
-        echo "-d - Disabled flag. 1 for disabled, 0 for enabled. Default value is 0."
-        echo "-a - Active flag. 1 for active 0 for inactive. Default value is 1."
+        echo "-g - Group name. Requires quotes if more than one word."
         echo
         echo "Examples:"
-        echo "./UserAPIs.sh -o show-users"
-        echo "./UserAPIs.sh -o create-user -u Username -f \"First Name\" -l \"Last Name\" -p Password -t Type -r Role"
-        echo "./UserAPIs.sh -o edit-user -u Username -f \"First Name\" -l \"Last Name\" -p Password -r Role -e Email -d [0/1] -a [0/1]"
+        echo "./GroupAPIs.sh -o get-all-groups"
+        echo "./GroupAPIs.sh -o add-to-group -u Username -g \"Group Name\""
+        echo
         exit 2
 
 
 }
 
-Disable=0
-Active=1
 
-while getopts "o:u:g:l:p:e:t:r:d:a:h" opt
+while getopts "o:u:g:h" opt
 do
         case ${opt} in
                 o) OPTION=$OPTARG ;;
                 u) USER=$OPTARG ;;
                 g) Group=$OPTARG ;;
-                l) LastName=$OPTARG ;;
-                p) Password=$OPTARG ;;
-                e) Email=$OPTARG ;;
-                t) Type=$OPTARG ;;
-                r) Role=$OPTARG ;;
-                d) Disable=$OPTARG ;;
-                a) Active=$OPTARG ;;
                 h) usage ;;
         esac
 done
